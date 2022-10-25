5e8158acb57b96d54ba86ec77aaeeb74b2a76a27
jim2832 <jim880705@gmail.com>
2022-10-25 15:01:52 +0800

    221025 update

---
+++ HW/HW1/matrix_mul.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 4
+
+void show_matrix(int matrix[N][N]);
+
+int main(void){
+    int A[N][N], B[N][N], C[N][N];
+
+    //init
+    /*
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            A[i][j] = 0;
+            B[i][j] = 0;
+            C[i][j] = 0;
+        }
+    }
+    */
+
+    //input
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    //multiplication
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            C[i][j] = 0;
+            for(int k=0; k<N; k++){
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    //show C matrix
+    show_matrix(C);
+
+    return 0;
+}
+
+void show_matrix(int matrix[N][N]){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            printf("%-5d", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
